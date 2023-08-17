9c5330da6ae1c15f51e4786161f22f12fb065cf7
Adebowale Adewale <adewalewale55@gmail.com>
2023-08-16 17:59:05 -0700

    Print _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This repository contains solutions to my task for C-Functions and nested loops at ALX SE
+0-putchar.c: C program that prints _putchar, followed by a new line
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * File: main.h
+ *
+ * Description: Header file containing declarations for all functions
+ *              used in 0x02-functions_nested_loops.
+ */
+
+	int _putchar (char c);
+#endif
