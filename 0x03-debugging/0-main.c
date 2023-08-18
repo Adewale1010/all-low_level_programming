f4059463e07040e85aeaaf0120df809f8cb1e1a7
Adebowale Adewale <adewalewale55@gmail.com>
2023-08-17 19:52:06 -0700

    Test if the function that prints if an integer is positive or negative, gives the correct output if the given case is 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if integer is positive of negative,
+ *        gives the correct output when the given case is 0.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This repository contains my  solutions to the debugging task at ALX SE
+0-main.c: C program that Tests if the function that prints if an integer is positive or negative, gives the correct output if the given case is 0
