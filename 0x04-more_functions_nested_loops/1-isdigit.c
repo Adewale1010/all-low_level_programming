b3ff09b44c18dac7cbfb5d26550c4af07aee5b5d
Adebowale Adewale <adewalewale55@gmail.com>
2023-08-18 21:32:03 -0700

    Check for digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdidgit - Checks for a digit (0 through 9)
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is digit and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 Continuation on nested loops and functions in C
 0-isupper.c: C program that checks is a character is uppercase
+1-isdigit.c: C program that checks for digit (0 through 9)
