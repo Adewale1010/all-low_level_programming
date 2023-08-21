91d485481cd939de79263425f0975c9a2060e494
Adebowale Adewale <adewalewale55@gmail.com>
2023-08-21 04:06:22 -0700

    Print a square to the terminal with the character #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,28 @@
 #include "main.h"
 
 /**
- * print_square - Prints a square with the character #
- * @size: The size of the square.
+ * print_square - Prints a square to the terminal with the character #
+ * @size: The size of the square
  */
 
-void print_square(int size);
+void print_square(int size)
 {
-	int hght, widt;
+	int height, width;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (hght = 0; hght < size; hght++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (height = 0; height < size; height++)
 		{
-			for (widt = 0; widt < size; widt++)
+			for (width = 0; width < size; width++)
+			{
 				_putchar('#');
+			}
 
-			if (hght == size - 1)
-				continue;
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
