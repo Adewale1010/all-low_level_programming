5edbea26d1d1cd6b93061ffc141a8843775cc0aa
Adebowale Adewale <adewalewale55@gmail.com>
2023-08-21 04:35:53 -0700

    Print a Fizz-buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - Prints Fizz-buzz test
@@ -11,15 +11,15 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if ((num % 3) == 0 && (num % 5) == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (num % 3 == 0)
+		else if ((num % 3) == 0)
 		{
 			printf("Fizz");
 		}
-		else if (num % 5 == 0)
+		else if ((num % 5) == 0)
 		{
 			printf("Buzz");
 		}
