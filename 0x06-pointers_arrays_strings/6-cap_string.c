7a2e9ec75bfb9a8f7ff73ab4a606703eee192e68
Adebowale Adewale <adewalewale55@gmail.com>
2023-08-25 12:14:34 -0700

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,30 +2,36 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- * @n: The string to be capitalized.
+ * @s: The string to be capitalized.
  * Return: A pointer to the changed string.
  */
 
-char *cap_string(char *n)
+char *cap_string(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0;
 
-	if (*(n + count) >= 97 && *(n + count) <= 122)
-		*(n + count) = *(n + count) - 32;
-	count++;
-	while (*(n + count) != '\0')
+	while (s[i])
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(n + count) == sep_words[i])
-			{
-				if ((*(n + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(n + (count + 1)) = *(n + (count + 1)) - 32;
-				break;
-			}
-		}
-		count++;
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+
+		if (s[i - 1] == ' ' ||
+		    s[i - 1] == '\t' ||
+		    s[i - 1] == '\n' ||
+		    s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		    s[i - 1] == '.' ||
+		    s[i - 1] == '!' ||
+		    s[i - 1] == '?' ||
+		    s[i - 1] == '"' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == ')' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1] == '}' ||
+		    i == 0)
+			s[i] -= 32;
+		i++;
 	}
-	return (n);
+
+	return (s);
 }
