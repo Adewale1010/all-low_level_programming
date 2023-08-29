38da5300f047ee8d4a8bd947fc89a433497718f2
Adebowale Adewale <adewalewale55@gmail.com>
2023-08-28 17:30:39 -0700

    Fill the memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,11 @@
 char *_memset(char *s, char b, unsigned int n);
 {
 	unsigned int i;
-	unsigned char *memory = s, value = b;
 
 	for (i = 0; i < n; i++)
-		memory[i] = value;
+	{
+		s[i] = b;
+	}
 
-	return (memory);
+	return (s);
 }
