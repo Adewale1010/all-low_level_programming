d42d9db06840140675802854840d0d1d27b5906e
Adebowale Adewale <adewalewale55@gmail.com>
2023-09-14 14:16:47 -0700

    Print the result of simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * main - Prints the result of simple operations.
@@ -10,10 +8,10 @@
  * Return: Always 0.
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *op;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,24 +19,23 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	printf("%d\n", get_op_func(op)(num 1, num2));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
+	printf("%d\n", operation(num 1, num2));
 	return (0);
 }
